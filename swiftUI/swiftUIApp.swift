ab1cbbfc345e761a51bd930d0f7fe8efe54f4740
iOSRentel <88737803+iOSRentel@users.noreply.github.com>
2021-08-26 20:35:00 +0300

    Initial Commit

---
+++ swiftUI/swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIApp.swift
+//  swiftUI
+//
+//  Created by Boris Zinovyev on 26.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
